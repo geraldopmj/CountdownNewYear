3b57d74dc5ba68f6da6ef01ce481d47d005686a2
gerrarrrr <81118137+gerrarrrr@users.noreply.github.com>
2023-01-13 00:05:39 -0300

    1.3 added message on first day of the year

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-let newYear = new Date(2024, 0, 1, 0, 0, 0);
-
+let newYear = new Date(2023, 0, 1, 0, 0, 0);
 function addYears(date){//will add one year to var newYear when called
     date.setFullYear(date.getFullYear() + 1);
   };
 
-var myfunc = setInterval(function() { //function to calc how many days
-    var now = Date.now(); //gets today and now
-    var timeleft = newYear - now; //calculate how many time left to new year
-    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24)); //transform with math floor in days
-    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+let myfunc = setInterval(function() { //function to calc how many days
+    let now = Date.now(); //gets today and now
+    let timeleft = newYear - now; //calculate how many time left to new year
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24)); //transform with math floor in days
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
     document.querySelector('.days').innerHTML = days; //put the var result in the CSS selector
     document.querySelector('.hours').innerHTML = hours;
     document.querySelector('.minutes').innerHTML = minutes;
-    document.querySelector('.seconds').innerHTML = seconds;
+    document.querySelector('.seconds').innerHTML = seconds; 
 
-    if (timeleft <= 0){//call the function to update to next year
+    if (timeleft < -5){//call the function to update to next year
         addYears(newYear);
     };
+    
+    let mess = new Date(2024,00,01); //gets new year
+    let messMonth = mess.getMonth() +1;
+    let messDay = mess.getDate();
+    let today = new Date()
+    let nowMonth= today.getMonth() +1;
+    let nowDay= today.getDate();
+
+    if ((messMonth == nowMonth) && (messDay == nowDay)){//call the function to display Message
+        document.querySelector('.message').innerHTML = "Happy New Year!!!"; 
+    };
+
+    if ((messMonth == nowMonth) && (messDay < nowDay)){//call the function to display Message
+        addYears(mess); 
+    };
+
 
     }, 1)//set interval to 1 to milliseconds
+
