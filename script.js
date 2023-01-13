9a83f283e853c5be447e065e35b5aaa10e5bccaf
gerrarrrr <81118137+gerrarrrr@users.noreply.github.com>
2023-01-12 22:40:43 -0300

    1.2 refactored

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
-let newYear = new Date('2024-01-01');
+let newYear = new Date(2024, 0, 1, 0, 0, 0);
 
-function addYears(date) {//will add one year to var newYear when called
+function addYears(date){//will add one year to var newYear when called
     date.setFullYear(date.getFullYear() + 1);
   };
 
-function getTimeRemaining(endTime){
-    let t= Date.parse(endTime) - Date.parse(new Date());//sets the eventTime minus actual date
-    let seconds= Math.floor((t/1000)%60);//math.floor returns the largest integer less or equal to a given number
-    let minutes= Math.floor((t/1000/60)%60);
-    let hours= Math.floor((t/(1000*60*60))%24);
-    let days= Math.floor(t/(1000*60*60*24));
-    return{
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
+var myfunc = setInterval(function() { //function to calc how many days
+    var now = Date.now(); //gets today and now
+    var timeleft = newYear - now; //calculate how many time left to new year
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24)); //transform with math floor in days
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    document.querySelector('.days').innerHTML = days; //put the var result in the CSS selector
+    document.querySelector('.hours').innerHTML = hours;
+    document.querySelector('.minutes').innerHTML = minutes;
+    document.querySelector('.seconds').innerHTML = seconds;
+
+    if (timeleft <= 0){//call the function to update to next year
+        addYears(newYear);
     };
-}
 
-function initializeClock(clockId, endTime){// initialize the clock
-    let clock = document.getElementById(clockId);
-    let daysSpan = clock.querySelector('.days');//querySelector return first element that matches te specified  CSS selector(s)
-    let hoursSpan = clock.querySelector('.hours');
-    let minutesSpan = clock.querySelector('.minutes');
-    let secondsSpan = clock.querySelector('.seconds');
-
-    function updateClock(){//updates the clock
-        var t =  getTimeRemaining(endTime);
-
-        daysSpan.innerHTML = t.days;
-        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-        
-        if (t.total<=0){//call the function to update to next year
-            addYears(newYear);
-        }
-    }
-    updateClock();
-    var timeInterval = setInterval(updateClock, 1);
-}
-initializeClock('clockDiv', newYear)
+    }, 1)//set interval to 1 to milliseconds
